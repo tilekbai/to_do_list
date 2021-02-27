3c1dbdcfda3e24520523c4d6540789365c1cd839
Tilekbai <tilek009@mail.ru>
2021-02-27 20:35:20 +0900

    Создание модели: "Problem"

--- source/to_do_list/models.py
+++ source/to_do_list/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Problem(models.Model):
+    description = models.TextField(max_length=3000, null=False)
+    status = models.CharField(max_length=50, null=False, default="New")
+    dead_line = models.DateField()
+
+    class Meta:
+        db_table = "problems"
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
+
+
+    def __str__(self):
+        return f'{self.id}. {self.description}. {self.status}: {self.dead_line}'
