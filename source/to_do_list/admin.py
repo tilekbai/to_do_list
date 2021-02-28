4b62ecedffa1a01403aba340eb85334e4dda15ac
Tilekbai <tilek009@mail.ru>
2021-02-28 17:07:15 +0900

    Регистрация в панели администратора модели: "Problem"

--- source/to_do_list/admin.py
+++ source/to_do_list/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from to_do_list.models import Problem
 
 # Register your models here.
+
+class ProblemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'dead_line']
+    list_filter = ['description', 'status']
+    search_fields = ['description', 'status']
+    fields = ['description', 'status', 'dead_line']
+
+
+admin.site.register(Problem, ProblemAdmin)
