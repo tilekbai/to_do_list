95eb3fed3eeea0e8e0c335deb26a053361a4786f
Tilekbai <tilek009@mail.ru>
2021-02-28 21:24:51 +0900

    Создание "index_view" в "views.py"

--- source/to_do_list/views.py
+++ source/to_do_list/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from to_do_list.models import Problem
 
 # Create your views here.
+
+def index_view(request):
+    problems = Problem.objects.all()
+    return render(request, 'index.html', context={'problems': problems})
