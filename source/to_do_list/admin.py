a2b041764ec6f47aea6048e63f6f0f54d2a8752e
Tilekbai <tilek009@mail.ru>
2021-03-01 21:06:06 +0900

    Добавление поля-"detailed_description" в "admin.py"

--- source/to_do_list/admin.py
+++ source/to_do_list/admin.py
@@ -4,10 +4,10 @@ from to_do_list.models import Problem
 # Register your models here.
 
 class ProblemAdmin(admin.ModelAdmin):
-    list_display = ['id', 'description', 'status', 'dead_line']
+    list_display = ['id', 'description', 'detailed_description', 'status', 'dead_line']
     list_filter = ['description', 'status']
     search_fields = ['description', 'status']
-    fields = ['description', 'status', 'dead_line']
+    fields = ['description', 'detailed_description', 'status', 'dead_line']
 
 
 admin.site.register(Problem, ProblemAdmin)
