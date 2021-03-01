623f322b83b30f199d17b06d403aa0745bb00486
Tilekbai <tilek009@mail.ru>
2021-03-01 21:00:29 +0900

    Добавление поля-"detailed_description" в модель-"Problem" и генерация миграций-"0003_problem_detailed_description.py"

--- source/to_do_list/models.py
+++ source/to_do_list/models.py
@@ -4,9 +4,10 @@ status_choice = [("new", "Новая"), ("in_progress", "В процессе"), 
 # Create your models here.
 
 class Problem(models.Model):
-    description = models.TextField(max_length=3000, null=False)
+    description = models.TextField(max_length=3000, null=False, blank=False)
+    detailed_description = models.TextField(max_length=7000, null=True, blank=True)
     status = models.CharField(max_length=30, choices=status_choice, null=False, default="new")
-    dead_line = models.DateField()
+    dead_line = models.DateField(blank=False)
 
     class Meta:
         db_table = "problems"
@@ -15,4 +16,4 @@ class Problem(models.Model):
 
 
     def __str__(self):
-        return f'{self.id}. {self.description}. {self.status}: {self.dead_line}'
+        return f'{self.id}. {self.description}. {self.detailed_description}. {self.status}: {self.dead_line}'
